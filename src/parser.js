da93d5fba0dfa591a6d21cd9e8d3839ec1c6a973
Tyler Nickerson <nickersoft@gmail.com>
2017-12-05 13:00:14 -0500

    Upgraded dependencies

--- src/parser.js
+++ src/parser.js
@@ -1085,7 +1085,8 @@ var parser = (function() {
                 if (next.length < 20) {
                     next += this._input.substr(0, 20 - next.length);
                 }
-                return (next.substr(0, 20) + (next.length > 20 ? '...' : '')
+                return (
+                    next.substr(0, 20) + (next.length > 20 ? '...' : '')
                 ).replace(/\n/g, '');
             },
 
