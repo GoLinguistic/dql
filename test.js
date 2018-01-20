888dc32b5423dbde525a340c645dbc2da5c6545a
Tyler Nickerson <nickersoft@gmail.com>
2018-01-20 17:33:41 -0500

    Added support for test running without rebuilding the parser

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
+import path from 'path';
 import test from 'ava';
 import yaml from 'js-yaml';
+import { Parser } from 'jison';
 
 // Test all exports
 test('PostgresQL flavor exported correctly', t =>
@@ -10,8 +12,13 @@ test('MSSQL flavor exported correctly', t => t.truthy(require('./src').mssql));
 test('Parser exported correctly', t => t.truthy(require('./src').parser));
 
 // Test query generation
-import { parser, postgres as dql } from './src';
+import { postgres as dql } from './src';
 
+const grammar = fs.readFileSync(
+    path.join(__dirname, './cfg/grammar.jison'),
+    'utf8'
+);
+const parser = new Parser(grammar);
 const tests = yaml.safeLoad(fs.readFileSync('tests.yml', 'utf8'));
 
 test('Should handle preprocessed data correctly', t => {
@@ -27,11 +34,13 @@ tests.forEach(entry => {
     const config = entry.config || {};
 
     if (!entry.expected)
-        test(entry.name, t => t.throws(() => dql`${entry.document}`()));
+        test(entry.name, t =>
+            t.throws(() => dql(parser.parse(entry.document))())
+        );
     else
         test(entry.name, t =>
             t.is(
-                dql`${entry.document}`(config, true),
+                dql(parser.parse(entry.document))(config, true),
                 entry.expected.replace(/\s+/g, ' ').trim()
             )
         );
